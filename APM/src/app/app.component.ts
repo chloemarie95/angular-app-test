06a5eff1b36c8bec784f5ff2238b34f1de0cfd10
McDermott <P10471139@capita.co.uk>
2019-10-30 17:51:52 +0000

    added packages and began product-list component

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
+  selector: 'pm-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Angular: Getting Started';
 }
+
+
